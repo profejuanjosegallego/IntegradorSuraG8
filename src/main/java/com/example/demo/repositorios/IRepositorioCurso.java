6759850733b071a79a3fa9b5762e14ba04785d7e
Andrey <andrey2015monsalve@gmail.com>
2026-02-03 16:31:23 -0500

    feat se crea repositorio de cursos

---
+++ src/main/java/com/example/demo/repositorios/IRepositorioCurso.java
@@ -0,0 +1,7 @@
+package com.example.demo.repositorios;
+
+import com.example.APISURAG8CURSOS.modelos.Curso;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IrepositorioCurso extends JpaRepository<Curso,Integer> {
+}
